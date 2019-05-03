439fb61e1378a25cf2d5a87084ee9537d535efc8
kentana20 <civic.b16b@gmail.com>
2019-05-03 13:22:50 +0900

    add geometry

---
+++ ch6/geometry/geometry.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct {
+	X, Y float64
+}
+
+func Distance(p, q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+func (p Point) Distance(q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+func main() {
+	a := Point{1.0, 2.0}
+	b := Point{10.0, 20.0}
+
+	fmt.Printf("distance is %f\n", Distance(a, b))
+}
