4cc3eed2b9d9cd9dba292c63a7f98e0ff3b99bf7
kentana20 <civic.b16b@gmail.com>
2019-05-24 17:54:59 +0900

    add 7-12

---
+++ ch7/7-12/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+var list = template.Must(template.New("list").Parse(`
+<!DOCTYPE html>
+<html>
+<body>
+<table>
+<tr>
+<td>item</td>
+<td>price</td>
+</tr>
+<tr>
+{{range $item, $price := .}}
+<td>{{$item}}</td>
+<td>{{$price}}</td>
+</tr>
+{{end}}
+</table>
+</body>
+</html>
+`))
+
+type dollars float32
+
+func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
+
+func main() {
+	db := database{"shoes": 50, "socks": 5}
+	mux := http.NewServeMux()
+	mux.Handle("/list", http.HandlerFunc(db.list))
+	mux.Handle("/price", http.HandlerFunc(db.price))
+	mux.Handle("/update", http.HandlerFunc(db.update))
+	mux.Handle("/delete", http.HandlerFunc(db.delete))
+	log.Fatal(http.ListenAndServe("localhost:8000", mux))
+}
+
+type database map[string]dollars
+
+func (db database) list(w http.ResponseWriter, req *http.Request) {
+	list.Execute(w, db)
+	// for item, price := range db {
+	// 	fmt.Fprintf(w, "%s: %s\n", item, price)
+	// }
+}
+
+func (db database) price(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound) // 404
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+	fmt.Fprintf(w, "%s\n", price)
+}
+
+func (db database) update(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	uPrice, _ := strconv.ParseFloat(req.URL.Query().Get("price"), 32)
+	price, ok := db[item]
+	if ok {
+		db[item] = dollars(uPrice)
+	} else {
+		w.WriteHeader(http.StatusNotFound) // 404
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+	fmt.Fprintf(w, "%s price is updated. %s -> %s \n", item, price, dollars(uPrice))
+}
+
+func (db database) delete(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	_, ok := db[item]
+	if ok {
+		delete(db, item)
+	} else {
+		w.WriteHeader(http.StatusNotFound) // 404
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+	fmt.Fprintf(w, "%s is deleted.\n", item)
+}
